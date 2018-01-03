d119cc6336f18026fa4199653e9a65a9bca1b6bc
Larry Li <larry.zy.li@gmail.com>
2018-01-03 15:52:54 -0600

    finished all debugging

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,10 +1,9 @@
-import './Home.css';
+import React, { Component } from "react";
+import "./Home.css";
 
 class Home extends Component {
     render() {
-        return (
-            <div className="home-landing"></div>
-        )
+        return <div className="home-landing" />;
     }
 }
 
--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getAllProducts = function() {
-    return axios.get("https://practiceapi.devmountain.com/products/")
-    .then(response => response.data);
-}
+    return axios
+        .get("https://practiceapi.devmountain.com/products/")
+        .then(response => response.data);
+};
+
+export default { getAllProducts };
